94930295152d108f87fbe37cfad2a65f81d6324c
Kim Seung Gu <agapeuni@naver.com>
2024-04-07 05:28:07 +0900

    init commit

---
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -0,0 +1,130 @@
+package com.mysite.sbb;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+import com.mysite.sbb.question.QuestionService;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+class SbbApplicationTests {
+
+	@Autowired
+	private QuestionRepository questionRepository;
+
+	@Autowired
+	private AnswerRepository answerRepository;
+	
+	@Autowired
+	private QuestionService questionService;
+
+	@Test
+	void testJpa() {
+		for (int i = 1; i <= 300; i++) {
+			String subject = String.format("테스트 데이터입니다:[%03d]", i);
+			String content = "내용무";
+			this.questionService.create(subject, content);
+		}
+	}
+
+	@Test
+	@Order(1)
+	void testClear() {
+		List<Question> all = this.questionRepository.findAll();
+		for (Question q : all) {
+			this.questionRepository.delete(q);
+		}
+	}
+
+	@Test
+	@Order(2)
+	void testSave() {
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1); // 첫번째 질문 저장
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2); // 두번째 질문 저장
+	}
+
+	@Test
+	@Order(3)
+	void testFindAll() {
+		List<Question> all = this.questionRepository.findAll();
+		Question q = all.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+
+		assertEquals(2, this.questionRepository.count());
+	}
+
+	@Test
+	@Order(4)
+	void testFindById() {
+		Optional<Question> oq = this.questionRepository.findById(1);
+		if (oq.isPresent()) {
+			Question q = oq.get();
+			assertEquals("sbb가 무엇인가요?", q.getSubject());
+		}
+	}
+
+	@Test
+	@Order(5)
+	void testFindBySubject() {
+		Question q1 = this.questionRepository.findBySubject("sbb가 무엇인가요?");
+		assertEquals("sbb에 대해서 알고 싶습니다.", q1.getContent());
+	}
+
+	@Test
+	@Order(6)
+	void testList() {
+		List<Question> qList = this.questionRepository.findBySubjectLike("sbb%");
+		Question q = qList.get(0);
+		assertEquals("sbb가 무엇인가요?", q.getSubject());
+	}
+
+	@Test
+	@Order(7)
+	void testUpdate() {
+		Optional<Question> oq = Optional.ofNullable(this.questionRepository.findBySubject("sbb가 무엇인가요?"));
+		// Optional<Question> oq = this.questionRepository.findById(1);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+		q.setSubject("수정된 제목");
+		this.questionRepository.save(q);
+	}
+
+	@Test
+	@Order(8)
+	void testQnA() {
+		Optional<Question> oq = Optional.ofNullable(this.questionRepository.findBySubject("수정된 제목"));
+		// Optional<Question> oq = this.questionRepository.findById(2);
+		assertTrue(oq.isPresent());
+		Question q = oq.get();
+
+		Answer a = new Answer();
+		a.setContent("네 자동으로 생성됩니다.");
+		a.setQuestion(q); // 어떤 질문의 답변인지 알기위해서 Question 객체가 필요하다.
+		a.setCreateDate(LocalDateTime.now());
+		this.answerRepository.save(a);
+	}
+}
